61988f8ae70d09f13dd6407adf1dbae6545dc4a1
Felipe300 <caracoloro@gmail.com>
2024-02-05 19:33:13 -0300

    refactor: delete unused keybindigs

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -7,16 +7,9 @@ return {
   end,
   config = function()
     local keymap = vim.keymap
-    keymap.set("n", "<leader>mp", ":MarkdownPreview<cr>", { buffer = 0, noremap = true, desc = "Markdown Preview" })
     keymap.set(
       "n",
-      "<leader>ms",
-      ":MarkdownPreviewStop<cr>",
-      { buffer = 0, noremap = true, desc = "Markdown Preview Stop" }
-    )
-    keymap.set(
-      "n",
-      "<leader>mt",
+      "<leader>wm",
       ":MarkdownPreviewToggle<cr>",
       { buffer = 0, noremap = true, desc = "Markdown Preview Toggle" }
     )
--- lua/plugins/twilight.lua
+++ lua/plugins/twilight.lua
@@ -5,8 +5,6 @@ return {
   },
   config = function()
     local keymap = vim.keymap
-    keymap.set("n", "<leader>tp", ":PencilHard<cr> | :Twilight<cr>", { desc = "Twilight + Pencil" })
-    keymap.set("n", "<leader>te", ":TwilightEnable<cr>", { noremap = true, desc = "Twilight Enable" })
-    keymap.set("n", "<leader>td", ":TwilightDisable<cr>", { noremap = true, desc = "Twilight Disable" })
+    keymap.set("n", "<leader>uP", ":PencilHard<cr> | :Twilight<cr>", { desc = "Twilight + Pencil" })
   end,
 }
