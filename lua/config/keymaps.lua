b7dd1708518d6ac7325d5e4ee89754da4111f01b
Felipe300 <caracoloro@gmail.com>
2024-02-24 20:14:44 -0300

    feat: replace winbar for incline.nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,10 +1,5 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-vim.keymap.set(
-  "n",
-  "<leader>sx",
-  require("telescope.builtin").resume,
-  { noremap = true, silent = true, desc = "Resume" }
-)
-vim.keymap.set("n", "Y", "y$", { desc = "Yank to the end of line" })
+local key = vim.keymap
+
+key.set("n", "<leader>sx", require("telescope.builtin").resume, { noremap = true, silent = true, desc = "Resume" })
+key.set("n", "Y", "y$", { desc = "Yank to the end of line" })
+key.set("n", "x", '"_x', { desc = "Cut out single char & send it to unmaped register" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,28 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-vim.opt.winbar = "%=%m %f"
+local opts = vim.opt
 
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+vim.scriptencoding = "utf-8"
+
+-- opts.winbar = "%=%m %f"
+
+opts.encoding = "utf-8"
+opts.fileencoding = "utf-8"
+
+opts.title = true
+opts.autoindent = true
+opts.hlsearch = true
+opts.backup = false
+opts.showcmd = true
+opts.cmdheight = 0
+opts.laststatus = 0
+opts.scrolloff = 10
+opts.inccommand = "split"
+opts.smarttab = true
+opts.breakindent = true
+opts.backspace = { "start", "eol", "indent" }
+opts.path:append({ "**" })
+opts.wildignore:append({ "*/node_modules/*" })
+-- -- Add asterisks in block comments
+opts.formatoptions:append({ "r" })
