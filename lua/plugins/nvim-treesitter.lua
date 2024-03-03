60e4b4d632a997c9790684af0a4af52e2bd6d61a
Felipe300 <caracoloro@gmail.com>
2024-03-03 18:33:45 -0300

    feat: add gitignore

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,58 +1,44 @@
----@diagnostic disable: missing-fields
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPre", "BufNewFile" },
     build = ":TSUpdate",
     dependencies = { "windwp/nvim-ts-autotag" },
-    config = function()
-      local treesitter = require("nvim-treesitter.configs")
-
-      treesitter.setup({
-        indent = { enable = true },
-        autotag = { enable = true },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlight = { "markdown" },
-        },
-        ensure_installed = {
-          "astro",
-          "bash",
-          "css",
-          "graphql",
-          "gitignore",
-          "html",
-          -- "htmx",
-          "javascript",
-          "json",
-          "jsonc",
-          "kdl",
-          "lua",
-          "markdown",
-          "markdown_inline",
-          "prisma",
-          "query",
-          "rust",
-          "tsx",
-          "typescript",
-          "vim",
-          "vimdoc",
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>", -- <leader>ss
-            node_incremental = "<C-space>", -- <leader>si
-            scope_incremental = false, -- <leader>sc
-            node_decremental = "<bs>", -- <leader>sd
-          },
-        },
-        query_linter = {
-          enable = true,
-          use_virtual_text = true,
-          lint_events = { "BufWrite", "CursorHold" },
-        },
-      })
-    end,
+    main = "nvim-treesitter.configs",
+    opts = {
+      indent = { enable = true },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlight = { "markdown" },
+      },
+      ensure_installed = {
+        "astro",
+        "bash",
+        "css",
+        "graphql",
+        "gitignore",
+        "html",
+        -- "htmx",
+        "javascript",
+        "json",
+        "jsonc",
+        "kdl",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "prisma",
+        "query",
+        "rust",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+      },
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
+    },
   },
 }
